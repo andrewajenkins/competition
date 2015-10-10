be8d2ea73e810b1cf3f21b58ff109e31942bad9f
Andrew Jenkins <AndrewJenkinsSD@gmail.com>
2015-10-10 14:35:14 -0500

    add BFS and vertex object

---
+++ src/algo/objects/Vertex.java
@@ -0,0 +1,15 @@
+package objects;
+
+public class Vertex {
+    public int data;
+    public int c; //color
+    public int d; //distance
+    public int t; //time
+    public Vertex p; //predecessor
+
+    @Override
+    public String toString() {
+        return "data: " + data + "color: " + c + " distance: "
+                + d + " predecessor: " + p.data;
+    }
+}
