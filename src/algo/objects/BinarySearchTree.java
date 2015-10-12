92452c0c5b49e717572ff9ed24ef8b88b1786e6d
Andrew Jenkins <AndrewJenkinsSD@gmail.com>
2015-10-11 23:44:48 -0500

    add bst iterative search

--- src/algo/objects/BinarySearchTree.java
+++ src/algo/objects/BinarySearchTree.java
@@ -2,6 +2,17 @@ package objects;
 
 public class BinarySearchTree {
 
+    public static TreeNode iterativeTreeSearch(TreeNode x, int k) {
+        while(x != null && k != x.key) {
+            if(k < x.key) {
+                x = x.left;
+            } else {
+                x = x.right;
+            }
+        }
+        return x;
+    }
+
     public static TreeNode treeSearch(TreeNode x, int k) {
         if(x == null || k == x.key) {
             return x;
