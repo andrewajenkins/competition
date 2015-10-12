565bc253bfe514429994596cd8cb7b23bdc06075
Andrew Jenkins <AndrewJenkinsSD@gmail.com>
2015-10-11 23:02:04 -0500

    add primitive hashtable

--- src/algo/objects/HashTable.java
+++ src/algo/objects/HashTable.java
@@ -1,20 +1,45 @@
 package objects;
 
-import java.util.LinkedList;
-import java.util.List;
-
-/**
- * Created by username on 10/11/2015.
- */
 public class HashTable {
 
-    List<Integer> table = new LinkedList<>();
+    private static LinkedList[] table = new LinkedList[256];
 
     public void chainedHashInsert(HashTable t, int x) {
-        t.table.add(0, x);
+        LinkedList.listInsert(t.table[h(x)], new Node(x));
     }
 
-    public void chainedHashSearch(HashTable t, int k) {
+    public boolean chainedHashSearch(HashTable t, int k) {
+        LinkedList list = t.table[h(k)];
+        Node node = list.head;
+        while(node.next != null) {
+            if(node.key == k) return true;
+            node = node.next;
+        }
+        return false;
+    }
 
+    public void chainedHashDelete(HashTable t, int k) {
+        LinkedList list = t.table[h(k)];
+        LinkedList.listDelete(list, new Node(k));
+    }
+
+    public static int h(int k) {
+        return divisionHash(k);
+    }
+
+    public static int divisionHash(int k) {
+        return k % table.length;
+    }
+
+    public static int multiplicationHash(int k) {
+        int result = 17;
+        result = 31 * result + k;
+        return result % table.length;
+    }
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 256; i++) {
+            System.out.println(multiplicationHash(i));
+        }
     }
 }
--- src/algo/objects/Node.java
+++ src/algo/objects/Node.java
@@ -4,4 +4,7 @@ public class Node {
     int key;
     Node next;
     Node prev;
+    public Node(int k) {
+        this.key = k;
+    }
 }
