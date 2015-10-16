dd8487aab9d762190c1688ce4de4322a71296d7c
Andrew Jenkins <AndrewJenkinsSD@gmail.com>
2015-10-16 00:22:21 -0500

    floyd warshall done

--- SRM 661 DIV 2/src/BridgeBuildingDiv2.java
+++ SRM 661 DIV 2/src/BridgeBuildingDiv2.java
@@ -6,21 +6,47 @@ public class BridgeBuildingDiv2 {
 	
 	public int minDiameter(int[] a, int[] b, int K) {
 
-		int n = 2 * (a.length - 1);
+		int l = a.length;
+
+		int n = 2 * l;
 
 		int[] c = new int[2 * n];
 
 		System.arraycopy(a, 0, c, 0, a.length);
 		System.arraycopy(b, 0, c, a.length, b.length);
 
-		int[][] dist = new int[n][n];
+		int[][] dist = new int[n+1][n+1];
 
-		for(int i = 0; i < n - 1; i++) {
-			dist[i][i+1] = c[i];
-			dist[i+1][i] = c[i];
+		for(int[] row : dist) {
+			Arrays.fill(row, 999);
+		}
+
+		dist[0][0] = dist[l][l] = 0;
+		for(int i = 0; i < l; i++) {
+			dist[i][i+1] = a[i];
+			dist[i+1][i] = a[i];
+			dist[i+l][i+l+1] = b[i];
+			dist[i+l+1][i+l] = b[i];
+			dist[i+1][i+1] = 0;
+			dist[i+l+1][i+l+1] = 0;
 		}
 
 		String[] sa = Arrays.deepToString(dist).split("]");
+		for(String s : sa) {
+			System.out.println(s);
+		}
+
+		for(int k = 0; k < n+1; k++) {
+			for(int s = 0; s < n+1; s++) {
+				for(int d = 0; d < n+1; d++) {
+					if(dist[s][k] + dist[k][d] < dist[s][d]) {
+						dist[s][d] = dist[s][k] + dist[k][d];
+					}
+				}
+			}
+		}
+
+		sa = Arrays.deepToString(dist).split("]");
 		for(String s : sa) {
 			System.out.println(s);
 		}
