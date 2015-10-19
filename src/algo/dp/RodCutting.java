f72670b150c4ecd9e74a7bfb8aeb260645677539
Andrew Jenkins <AndrewJenkinsSD@gmail.com>
2015-10-19 00:00:43 -0500

    add memoized rod cut

--- src/algo/dp/RodCutting.java
+++ src/algo/dp/RodCutting.java
@@ -1,5 +1,7 @@
 package dp;
 
+import java.util.Arrays;
+
 /**
  * Created by username on 10/16/2015.
  */
@@ -13,6 +15,30 @@ public class RodCutting {
         int q = Integer.MIN_VALUE;
         for(int i = 1; i <= n; i++) {
             q = Math.max(q, p[i] + cutRod(p, n-1));
+        }
+        return q;
+    }
+
+    public static int memoizedCutRod(int[] p, int n) {
+        int[] array = new int[n];
+        Arrays.fill(array, Integer.MIN_VALUE);
+        return memoizedCutRodAux(p, n, array);
+    }
+
+    public static int memoizedCutRodAux(int[] p, int n, int[] r) {
+        int q = 0;
+        if(r[n] >= 0) {
+            return r[n];
+        }
+        if(n == 0) {
+            q = 0;
+        }
+        else {
+            q = Integer.MIN_VALUE;
+            for(int i = 0; i < n; i++) {
+                q = Math.max(q, p[i] + memoizedCutRodAux(p, n-i, r));
+            }
+            r[n] = q;
         }
         return q;
     }
