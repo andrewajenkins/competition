ce4d5693df9772ae0e34fe2095e3b50450e66d15
Andrew Jenkins <AndrewJenkinsSD@gmail.com>
2015-10-19 20:30:34 -0500

    add steps and steps memoized

---
+++ src/algo/dp/Steps.java
@@ -0,0 +1,27 @@
+package dp;
+
+public class Steps {
+    public int countWays(int n) {
+        if(n < 0) {
+            return 0;
+        } else if(n == 0) {
+            return 1;
+        } else {
+            return countWays(n-1) + countWays(n-2) + countWays(n-3);
+        }
+    }
+    public static int countWaysDP(int n, int[] map) {
+        if(n < 0) {
+            return 0;
+        } else if(n == 0) {
+            return 1;
+        } else if(map[n] > -1) {
+            return map[n];
+        } else {
+            map[n] = countWaysDP(n-1, map) +
+                    countWaysDP(n-2, map) +
+                    countWaysDP(n-3, map);
+            return map[n];
+        }
+    }
+}
