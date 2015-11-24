2fe26f5c1b7b1e7f56f6afb48b51ff5b24e8c280
Andrew Jenkins <AndrewJenkinsSD@gmail.com>
2015-11-24 12:50:28 -0600

    CFR 602 while traversing an array for each value set a local min and max and traverse as long as diff of min and max not greater than 1 and store the longest sequence

---
+++ src/prob/CFR602/ApproxConstantRange.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+/*
+ * http://codeforces.com/contest/602/problem/B
+ */
+public class ApproxConstantRange {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.nextLine();
+		String[] ns = sc.nextLine().split(" ");
+		int result = -1;
+		for(int i = 0; i < ns.length; i++) {
+			int j = i;
+			int min = Integer.parseInt(ns[i]);
+			int max = Integer.parseInt(ns[i]);
+			while((max - min) <= 1 && ++j < ns.length) {
+				int val = Integer.parseInt(ns[j]);
+				if(val < min) min = val;
+				if(val > max) max = val;
+			}
+			result = Math.max(result, j-i);
+		}
+		System.out.println(result);
+		sc.close();
+	}
+}
