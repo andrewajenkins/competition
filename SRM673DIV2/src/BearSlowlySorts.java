dde0b87de59c2dca4c5d9081f99931bfe9d1e1e4
Andrew Jenkins <andrewjenkinssd@gmail.com>
2015-11-24 20:45:07 -0600

    srm672 bearslowlysorts just a bunch of special cases

--- SRM673DIV2/src/BearSlowlySorts.java
+++ SRM673DIV2/src/BearSlowlySorts.java
@@ -5,6 +5,24 @@ import static java.lang.Math.*;
 public class BearSlowlySorts {
 	
 	public int minMoves(int[] w) {
-		return 0;
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		for(int n : w) {
+			if(n > max) max = n;
+			if(n < min) min = n;
+		}
+		int first = w[0];
+		int last = w[w.length - 1];
+		if(first == max && last == min) return 3;
+		if(first != min && last != max) return 2;
+		if((first == max && last != min) || (first != max && last == min)) return 1;
+		if(first == min && last == max) {
+			int[] sorted = Arrays.copyOf(w, w.length);
+			Arrays.sort(sorted);
+			if(Arrays.equals(w, sorted)) {
+				return 0;
+			}
+		}
+		return 1;
 	}
 }
