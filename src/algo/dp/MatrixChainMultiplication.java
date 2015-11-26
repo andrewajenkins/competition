7d7f10889a1beb11554309baf05912d2e8099eb6
Andrew Jenkins <andrewjenkinssd@gmail.com>
2015-11-26 03:08:52 -0600

    add a generalized mxn matrix multiplication algorithm with test

--- src/algo/dp/MatrixChainMultiplication.java
+++ src/algo/dp/MatrixChainMultiplication.java
@@ -4,14 +4,38 @@ import java.util.Arrays;
 
 public class MatrixChainMultiplication {
 	public static void main(String[] args) {
+		//square matrix multiplication test
 		int[][] a = {{1, 2},{3, 4}};
 		int[][] b = {{5, 6},{7, 8}};
 		int[][] r = squareMatrixMultiply(a, b);
-		for(int[] c : r) {
-			System.out.println(Arrays.toString(c));
+		System.out.println(Arrays.deepToString(r));
+
+		//mxn matrix multiplication test
+		int[][] c = {{1,2},{3,4},{5,6}};
+		int[][] d = {{1,2,3},{4,5,6}};
+		int[][] s = matrixMultiply(c, d);
+		System.out.println(Arrays.deepToString(s));
+	}
+
+	//generecized algorithm for matrix multiplication
+	private static int[][] matrixMultiply(int[][] a, int[][] b) {
+		if(a[0].length != b.length) {
+			throw new RuntimeException("incompatible dimensions");
+		} else {
+			int[][] c = new int[a.length][b[0].length];
+			for(int i = 0; i < a.length; i++) {
+				for(int j = 0; j < b[0].length; j++) {
+					c[i][j] = 0;
+					for(int k = 0; k < a[0].length; k++) {System.out.println(i+" "+j+" "+k);
+						c[i][j] += (a[i][k] * b[k][j]);
+					}
+				}
+			}
+			return c;
 		}
 	}
 
+	//Strassen's algorithm
 	private static int[][] squareMatrixMultiply(int[][] a, int[][] b) {
 		int n = a[0].length;
 		int[][] c = new int[n][n];
