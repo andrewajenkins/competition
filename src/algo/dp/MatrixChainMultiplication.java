3c3d99da25aa5ef034527bba8c0e3e09bf43c0ee
Andrew Jenkins <andrewjenkinssd@gmail.com>
2015-11-27 18:00:27 -0600

    add untested matrix chain order algorithm

--- src/algo/dp/MatrixChainMultiplication.java
+++ src/algo/dp/MatrixChainMultiplication.java
@@ -4,17 +4,42 @@ import java.util.Arrays;
 
 public class MatrixChainMultiplication {
 	public static void main(String[] args) {
-		//square matrix multiplication test
-		int[][] a = {{1, 2},{3, 4}};
-		int[][] b = {{5, 6},{7, 8}};
-		int[][] r = squareMatrixMultiply(a, b);
-		System.out.println(Arrays.deepToString(r));
+		
+//		runSquareMatrixTest();
+//		runRectangleMatrixTest();
+	
+	}
 
-		//mxn matrix multiplication test
-		int[][] c = {{1,2},{3,4},{5,6}};
-		int[][] d = {{1,2,3},{4,5,6}};
-		int[][] s = matrixMultiply(c, d);
-		System.out.println(Arrays.deepToString(s));
+	public ResultObject matrixChainOrder(int[] p) {
+		int n = p.length - 1;
+		int[][] m = new int[n][n];
+		int[][] s = new int[n][n];
+		for(int i = 0; i < n; i++) {
+			m[i][i] = 0;
+		}
+		for(int l = 2; l < n; l++) { //l is the chain length
+			for(int i = 1; i < n-l-1; i++) {
+				int j = i + l - 1;
+				m[i][j] = Integer.MAX_VALUE;
+				for(int k = i; k < j -1; k++) {
+					int q = m[i][k] + m[k+1][j] + (p[i-1]*p[k]*p[j]);
+					if(q < m[i][j]) {
+						m[i][j] = q;
+						s[i][j] = k;
+					}
+				}
+			}
+		}
+		return new ResultObject(m, s);
+	}
+
+	class ResultObject {
+		int[][] m;
+		int[][] s;
+		ResultObject (int[][] m, int[][] s) {
+			this.m = m;
+			this.s = s;
+		}
 	}
 
 	//generecized algorithm for matrix multiplication
@@ -48,5 +73,19 @@ public class MatrixChainMultiplication {
 			}
 		}
 		return c;
+	}
+
+	public static void runRectangleMatrixTest() {
+		int[][] c = {{1,2},{3,4},{5,6}};
+		int[][] d = {{1,2,3},{4,5,6}};
+		int[][] s = matrixMultiply(c, d);
+		System.out.println(Arrays.deepToString(s));
+	}
+
+	public static void runSquareMatrixTest() {
+		int[][] a = {{1, 2},{3, 4}};
+		int[][] b = {{5, 6},{7, 8}};
+		int[][] r = squareMatrixMultiply(a, b);
+		System.out.println(Arrays.deepToString(r));
 	}
 }
