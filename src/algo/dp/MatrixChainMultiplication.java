37adf8681b708b99c326530db4641016ace414b2
Andrew Jenkins <andrewjenkinssd@gmail.com>
2015-11-28 01:50:19 -0600

    add memoizedMatrixChain function to finish this class

--- src/algo/dp/MatrixChainMultiplication.java
+++ src/algo/dp/MatrixChainMultiplication.java
@@ -4,6 +4,33 @@ import java.util.Arrays;
 
 public class MatrixChainMultiplication {
 
+	public static int memoizedMatrixChain(int[] p) {
+		int n = p.length/2+1;
+		int[][] m = new int[n][n];
+		for(int i = 1; i < n; i++) {
+			for(int j = i; j < n; j++) {
+				m[i][j] = Integer.MAX_VALUE;
+			}
+		}
+		return lookupChain(m, p, 1, n - 1);
+	}
+
+	public static int lookupChain(int[][] m, int[] p, int i, int j) {
+		if(m[i][j] < Integer.MAX_VALUE)
+			return m[i][j];
+		if(i == j) 
+			m[i][j] = 0;
+		else {
+			for(int k = i; k <= j-1; k++) {
+				int q = lookupChain(m, p, i, k) + lookupChain(m, p, k + 1, j) + (p[i-1]*p[k]*p[j]);
+				if(q < m[i][j]) {
+					m[i][j] = q;
+				}
+			}
+		}
+		return m[i][j];
+	}
+
 	public static ResultObject matrixChainOrder(int[] p) {
 		int n = p.length/2 + 2;
 		int[][] m = new int[n][n];
@@ -86,6 +113,11 @@ public class MatrixChainMultiplication {
 		return c;
 	}
 
+	public static void runMemoizedMatrixChainTest() {
+		int[] a = {30,35,15,5,10,20,25,20,15,35,10,5};
+		System.out.println(memoizedMatrixChain(a));
+	}
+
 	public static void runMatrixChainOrderTest() {
 		int[] a = {30,35,15,5,10,20,25,20,15,35,10,5};
 		ResultObject ro = matrixChainOrder(a);
@@ -112,7 +144,8 @@ public class MatrixChainMultiplication {
 	}
 
 	public static void main(String[] args) {
-		runMatrixChainOrderTest();
+		runMemoizedMatrixChainTest();
+//		runMatrixChainOrderTest();
 //		runSquareMatrixTest();
 //		runRectangleMatrixTest();
 	
