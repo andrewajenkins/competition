2f63c432d3148fe84682142ecbd7f456e0292bfd
Andrew Jenkins <andrew_jenkins@homedepot.com>
2015-12-01 13:01:19 -0600

    partial work on srm 654 SquareScores

--- SRM654DIV2/src/SquareScoresDiv2.java
+++ SRM654DIV2/src/SquareScoresDiv2.java
@@ -5,6 +5,29 @@ import static java.lang.Math.*;
 public class SquareScoresDiv2 {
 	
 	public int getscore(String s) {
-		return 0;
+		char cur = ' ';
+		int counter = 1;
+		int sum = 0;
+		for(int i = 0; i < s.length(); i++) {
+			if(cur == s.charAt(i)) {
+				counter++;
+			} else {
+				sum += mult(counter);
+				System.out.println(counter+" "+cur+" makes new sum "+sum);
+				counter = 1;
+				cur = s.charAt(i);
+			}
+		}
+		return mult(s.length());
+	}
+
+	public int mult(int n) {
+		int mult = 1;
+		int sum = 0;
+		for(int i = n; i >= 0; i--) {
+			System.out.println(sum+" = "+mult+" * "+i);
+			sum += mult * i;
+		}
+		return sum;
 	}
 }
