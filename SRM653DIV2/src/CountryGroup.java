7c8736a1534bd702dcc8ce89918d79f0953c03c3
Andrew Jenkins <andrewjenkinssd@gmail.com>
2015-12-12 17:29:34 -0600

    SRM 635 country groupings

--- SRM653DIV2/src/CountryGroup.java
+++ SRM653DIV2/src/CountryGroup.java
@@ -1,10 +1,24 @@
 import java.util.*;
-import java.math.*;
-import static java.lang.Math.*;
 
-public class CountryGroup {
-	
-	public int solve(int[] a) {
-		return 0;
+public class CountryGroup
+{
+	public int solve(int[] a)
+	{
+		int cur = -1;
+		int counter = -1;
+		int total = 0;
+		for(int i = 0; i < a.length; i++) {
+			if(a[i] != cur || cur == 1) {
+				if(counter != cur) {
+					return -1;
+				}
+				total++;
+				cur = a[i];
+				counter = 1;
+			} else {
+				counter++;
+			}
+		}
+		return counter == cur ? total : -1;
 	}
 }
