936b74e8d6f78490babbc1614d6ac2ff4f785799
Andrew Jenkins <andrewjenkinssd@gmail.com>
2015-12-13 14:01:24 -0600

    fd: break player off into its own class

--- FanDuel/FanDuelKnapsack.java
+++ FanDuel/FanDuelKnapsack.java
@@ -41,29 +41,4 @@ public class FanDuelKnapsack {
 			System.out.println(data.get(id));
 		}
 	}
-
-	static class Player {
-		int id, played, salary;
-		String position, firstName, lastName, game, team,
-		       opponent, injury, deets;
-		Double rating;
-		Player(String[] sa) {
-			id = Integer.parseInt(sa[0]);
-			position = sa[1];
-			firstName = sa[2];
-			lastName = sa[3];
-			rating = Double.parseDouble(sa[4]);
-			played = Integer.parseInt(sa[5]);
-			salary = Integer.parseInt(sa[6]);
-			game = sa[7];
-			team = sa[8];
-			opponent = sa[9];
-			injury = sa[10];
-			deets = sa[11];
-		}
-		@Override
-		public String toString() {
-			return firstName + ", " + lastName;
-		}
-	}
 }
